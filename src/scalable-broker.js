1183df4288f0b42c344bba3d2e119a0908595570
jasineri <jasineri@gmx.de>
2021-04-29 15:50:59 +0200

    initial commit

--- src/scalable-broker.js
+++ src/scalable-broker.js
@@ -1,7 +1,12 @@
 /**
+ * The Scalable Broker web application does not have any functionality for exporting transaction lists, so this is my attempt to get one :)
+ *
+ * exportTransactions
+ *
  * Usage:
  * 1. Navigate to https://de.scalable.capital/broker/transactions
- * 2. Just copy and paste content of this script in the development console of your browser
+ * 2. Just copy and paste content of the scalable-broker.js into development console of your browser
+ * 3. Save the CSV
  *
  * @type {{exportTransactions: scalableBroker.exportTransactions}}
  */
